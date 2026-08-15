2d0e8711305d5ea881454e1a1cc0d92004221fb8
Diệp Gia Huy <30974088+diepgiahuy@users.noreply.github.com>
2026-08-15 18:24:56 +0700

    feat(provisioning): add browser SoftAP setup portal

---
+++ components/esp32_provisioning/include/companion/setup_portal.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "companion/provisioning_store.hpp"
+
+#include "esp_http_server.h"
+
+#include <array>
+#include <atomic>
+#include <string_view>
+
+namespace companion::provisioning {
+
+class SetupPortal final {
+public:
+  bool start(std::string_view device_suffix);
+  bool configured() const { return configured_.load(); }
+  bool take_pending(PendingConfig& out);
+
+  std::string_view ssid() const;
+  std::string_view password() const;
+
+private:
+  static esp_err_t handle_index(httpd_req_t* request);
+  static esp_err_t handle_configure(httpd_req_t* request);
+  esp_err_t configure(httpd_req_t* request);
+
+  httpd_handle_t server_{};
+  PendingConfig pending_{};
+  std::array<char, 33> ssid_{};
+  std::array<char, 17> password_{};
+  std::atomic<bool> configured_{false};
+};
+
+} // namespace companion::provisioning
