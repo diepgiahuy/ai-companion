63d271af808891289188ac37c00b1cedfae1fa8e
Diệp Gia Huy <30974088+diepgiahuy@users.noreply.github.com>
2026-08-16 00:46:36 +0700

    feat(onboarding): expose generated setup reference

--- components/esp32_provisioning/include/companion/setup_portal.hpp
+++ components/esp32_provisioning/include/companion/setup_portal.hpp
@@ -13,7 +13,7 @@ namespace companion::provisioning {
 
 class SetupPortal final {
 public:
-  bool start(std::string_view device_suffix);
+  bool start(std::string_view device_id, std::string_view device_suffix);
   bool start_wifi_only(std::string_view device_suffix);
   bool configured() const { return configured_.load(); }
   bool take_pending(PendingConfig& out);
@@ -23,15 +23,17 @@ public:
   std::string_view password() const;
 
 private:
-  bool start_impl(std::string_view device_suffix, bool wifi_only);
+  bool start_impl(std::string_view device_id, std::string_view device_suffix, bool wifi_only);
   static esp_err_t handle_index(httpd_req_t* request);
   static esp_err_t handle_nonce(httpd_req_t* request);
+  static esp_err_t handle_setup_info(httpd_req_t* request);
   static esp_err_t handle_configure(httpd_req_t* request);
   esp_err_t configure(httpd_req_t* request);
 
   httpd_handle_t server_{};
   PendingConfig pending_{};
   WifiConfig wifi_{};
+  std::array<char, 129> device_id_{};
   std::array<char, 33> ssid_{};
   std::array<char, 17> password_{};
   std::array<char, 33> session_nonce_{};
